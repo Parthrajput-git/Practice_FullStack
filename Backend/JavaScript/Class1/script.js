019975f0b5d198141efdae0b7d42f97ed1a00088
Parthrajput-git <parthrajput3834@gmail.com>
2026-07-28 16:11:35 +0530

    JavaScript practice

---
+++ Backend/JavaScript/Class1/script.js
@@ -0,0 +1,46 @@
+// console.log("Hello World");
+// let Name = "Nanu";
+
+// console.log(`My name is ${Name}`);
+let str="Hey programers";
+
+// let res=str.trim();
+
+// console.log(res);
+
+// let upper=str.toUpperCase();
+// console.log(upper);
+
+// let res=str.trim().toUpperCase();
+// console.log("result ==",res);
+
+// 
+// let result=str.repeat(5);
+// console.log(result);
+
+// console.log(str.trim().toUpperCase());
+
+// let arr=["Amma","Dev","Rohan","car","bike"];
+// // let arr1=["Mira",12,12.4];
+
+// console.log(arr.splice(4));
+
+// arr=["f","a","b","e","c"];
+
+for(let i=1;i<=15;i=i+2){
+    console.log(i);
+    
+}
+
+console.log("backword");
+
+// for(let i=15; i=>1;i=i-2){
+// console.log(i);
+
+// }
+
+
+
+
+
+
