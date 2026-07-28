dbb0742f08ea2f685d88ebeb3baae4822be39f5c
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-07-28 16:14:33 +0530

    Update script.js

--- Backend/JavaScript/Class1/script.js
+++ Backend/JavaScript/Class1/script.js
@@ -31,13 +31,12 @@ for(let i=1;i<=15;i=i+2){
     console.log(i);
     
 }
-
 console.log("backword");
 
-// for(let i=15; i=>1;i=i-2){
-// console.log(i);
+for(let i=15; i=>1;i=i-2){
+console.log(i);
 
-// }
+}
 
 
 
