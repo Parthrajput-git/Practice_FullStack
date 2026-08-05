138c562fb342a3d28656f23689a0d92adf5ec344
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-08-05 13:26:11 +0530

    Add files via upload

---
+++ Backend/JavaScript/Class1/Class3/script.js
@@ -0,0 +1,96 @@
+// let obj={
+//     nam:"hello",
+//     id:23
+// }
+// console.log(obj);
+
+// let student={
+//     name:"Prithvi",
+//     id:15,
+//     gmail:"prithvi@123",
+//     marks:455,
+//     subject:["Hindi","Math","English","Phy","Chem"]
+// }
+// console.log(student["gmail"]);
+// console.log(student.gmail);
+// console.log(student.subject);
+// console.log(student.subject[3]);
+
+
+// let student2={
+//     name:"Prithvi",
+//     id:15,
+//     gmail:"prithvi@123",
+//     marks:455,
+//     subject:["Hindi","Math","English","Phy","Chem"]
+// }
+// console.log(student2["gmail"]);
+// console.log(student2.gmail);
+// console.log(student2.subject);
+// console.log(student2.subject[3]);
+
+// console.log(student2.gender="Male");
+// console.log(student2.name="Prithvi Rajput");
+
+
+// console.log(delete student2.marks);
+// console.log(student2);
+
+// Object of Object
+// const classInfo = {
+//     class1: {
+//         name: "A+",
+//         Id: "First"
+//     },
+//     class2: {
+//         name: "B+",
+//         Id: "Second"
+//     },
+//     class3: {
+//         name: "C+",
+//         Id: "Third"
+//     }
+// }
+
+// console.log(classInfo);
+
+// //Array of object
+
+// const classInfo1 = [
+//     {
+//         name:  "A+",
+//         Id: "First"
+//     },
+//     {
+
+//         name:  "B+",
+//         Id: "Second"
+//     },
+//     {
+//         name:  "C+",
+//         Id: "Third"
+//     }
+// ]
+// console.log(classInfo1);
+// // object methods
+// console.log(Math.abs(-15));
+// // Power
+// console.log(Math.pow(4,2));
+
+// console.log(Math.floor(5.5));
+
+// console.log(Math.ceil(5.5));
+// console.log(Math.random());
+
+//operation1
+
+let step1=Math.random();
+ step1=step1*10;
+
+ step1=Math.floor(step1)
+console.log(step1);
+
+//operation2
+
+let num=Math.floor(Math.random()*10)+1;
+console.log(num);
