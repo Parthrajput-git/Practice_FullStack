25642e61bca938629a0a374e0162bad25a757b69
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-08-20 12:36:06 +0530

    Add files via upload

---
+++ Node/Class1/math.js
@@ -0,0 +1,12 @@
+
+// function some(){
+//     obj={
+//         name:"some",
+//         id:21,
+//         city: "indore"
+//     }
+// }
+
+// let value="Hello";
+
+// module.exports=value;
---
+++ Node/Class1/script.js
@@ -0,0 +1,24 @@
+// const pri=require("./math");
+// console.log(pri);
+
+// let n = 5;
+// for(let i=0;i<=5;i++){
+//     console.log("Hello",i);
+    
+// }
+// console.log("Bye");
+
+// let args=process.argv;
+
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello guys",args[i]);
+    
+// }
+
+let result=require("./Fruits");
+
+console.log(result);
+console.log(result[2]);
+
+
+
