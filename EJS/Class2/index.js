fcc54fdae65f114fca39faf245b096c2f857c40a
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-08-25 14:52:09 +0530

    Add files via upload

---
+++ EJS/Class2/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+let path=require("path");
+
+let port = 3000;
+
+app.set("views engine","ejs");
+
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/ig/:username",(req,res)=>{
+    let{username}=req.params;
+    res.render("instagram.ejs",{username}); 
+});
+
+app.get("/", (req, res) => {
+    res.send("This is  home page");
+});
+
+
+
+// app.get("/home",(req,res)=>{
+//   res.render("home.ejs")
+// });
+
+app.listen(port, () => {
+    console.log(`Linting request on port ${port}`);
+
+});
+
