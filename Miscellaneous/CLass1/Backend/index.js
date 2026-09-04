2733d9e8a0fdf24e04f47a09213898c1def524a7
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-09-04 21:02:52 +0530

    Update index.js

--- Miscellaneous/CLass1/Backend/index.js
+++ Miscellaneous/CLass1/Backend/index.js
@@ -3,13 +3,19 @@ const app = express();
 
 let port = 3000;
 
-app.get("/register",(req,res)=>{
-    let {user,name}=req.query;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/register", (req, res) => {
+    let { user, name } = req.query;
     res.send(`Standard GET response ,Welcome user ${user}`);
 });
 
-app.post("/register",(req,res)=>{
-    res.send("Standard POST response");
+app.post("/register", (req, res) => {
+    // console.log(req.body);
+    let {user,password}=req.body;
+    res.send(`This user is ${user}`);
+    // res.send("Standard POST response");
 });
 
 app.listen(port, () => {
