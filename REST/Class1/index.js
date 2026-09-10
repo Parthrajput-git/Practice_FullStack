44095aaeb42aedc750067c11770b840f1c107b13
@Parth Shingh Rajput <pr9410350@gmail.com>
2026-09-10 14:21:11 +0530

    Update index.js

--- REST/Class1/index.js
+++ REST/Class1/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
-const path=require("path");
+const path = require("path");
+const port = 8080;
+const { v4: uuidv4 } = require('uuid');
+const methodOverride=require("method-override");
 
-let port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use(express.json());
 
 app.set("view engine", "ejs");
@@ -12,25 +15,74 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let posts=[
+let posts = [
     {
+        id: uuidv4(),
         username: "John Doe",
         content: "This is a sample post content.",
     },
-     {
+    {
+        id: uuidv4(),
         username: "Miro comem",
         content: "I am a programmer and I love coding.",
     },
-     {
+    {
+        id: uuidv4(),
         username: "Sem vinore",
         content: "I am a student and I love learning.",
     }
 ]
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 
 app.get("/posts", (req, res) => {
-    res.render("index.ejs",{posts});
+    res.render("index.ejs", { posts });
+});
+
+
+app.get("/posts/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content })
+    res.redirect("/posts");
+    // res.send("Post requrest are working");
+
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    console.log(post);
+    res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content=newContent;
+    console.log(post);
+      res.redirect("/posts");
+    // res.send("Patch request are working :");
+});
+
+app.get("/posts/:id/edit",(req,res)=>{
+      let { id } = req.params;
+      let post = posts.find((p) => id === p.id);
+      res.render("edit.ejs",{ post });
+   
+});
+
+app.delete("/posts/:id",(req,res)=>{
+     let { id } = req.params;
+      posts = posts.filter((p) => id !== p.id);
+       res.redirect("/posts");
+    //   res.send("Delete are work");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
